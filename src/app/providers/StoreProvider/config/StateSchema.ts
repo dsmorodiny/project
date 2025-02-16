568c5e5faf64512e8783d170543cca94f83ad5a8
Andrei Rodionov <ya@dsmorodiny.ru>
2025-02-16 13:50:16 +0300

    add new input, add login, from ui

---
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -0,0 +1,7 @@
+import { CounterSchema } from 'entities/Counter';
+import { IUserSchema } from 'entities/User';
+
+export interface StateSchema {
+    counter: CounterSchema;
+    user: IUserSchema;
+}
